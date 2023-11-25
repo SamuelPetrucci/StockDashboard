f3f2f9f2af25cfab8af444aedddf93d53ca94df7
Samuel Petrucci <83541722+SamuelPetrucci@users.noreply.github.com>
2023-11-25 14:15:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import abs
+import streamlit as st
+
+# Writes the Sidebar label
+st.sidebar.write("Filters")
+
+# Page options
+options = ("Prediction Chart", "Stock data", "News Articles",)
+
+# Page selector
+option = st.sidebar.selectbox("Select a Dashboard", options=options)
+
+# Writes the option name as a header for each page
+st.header(option)
+
+# Determines the logic that will be used depending on the page that is selected
+if option == "Prediction Chart":
+    prediction_chart_page = abs.PredictionChartPage()
+    prediction_chart_page.display()
+
+if option == "Stock data":
+    stock_data_page = abs.StockDataPage()
+    stock_data_page.display()
+
+if option == "News Articles":
+    news_articles_page = abs.NewsArticlesPage()
+    news_articles_page.display()
+
